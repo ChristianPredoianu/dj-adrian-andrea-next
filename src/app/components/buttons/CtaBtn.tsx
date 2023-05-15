c9209f42e1f861e3e4ff7e70c11a4d9556a408c7
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-15 15:57:03 +0200

    added buttons folder

---
+++ src/app/components/buttons/CtaBtn.tsx
@@ -0,0 +1,7 @@
+export default function CtaBtn() {
+  return (
+    <button className='via-42.19% mt-6 inline-block cursor-pointer rounded-xl bg-gradient-to-b from-red-from-gradient/0 from-0% via-red-via-gradient to-red-to-gradient to-100% px-6 py-4 uppercase tracking-wider transition ease-in-out hover:scale-105 hover:via-red-500'>
+      Tracks
+    </button>
+  );
+}
