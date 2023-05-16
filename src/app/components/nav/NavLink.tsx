7d540a70a0e42b40638fbce3ba9830dc43c69922
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-16 15:43:10 +0200

    work

---
+++ src/app/components/nav/NavLink.tsx
@@ -0,0 +1,17 @@
+interface NavLinkProps {
+  link: {
+    name: string;
+  };
+  onCloseNavbar: () => void;
+}
+
+export default function NavLinks({ link, onCloseNavbar }: NavLinkProps) {
+  return (
+    <li
+      className='text-md transition-duration: 150ms z-50 flex items-center px-3 py-4 uppercase leading-snug tracking-widest  text-white transition-colors hover:opacity-75 '
+      onClick={onCloseNavbar}
+    >
+      {link.name}
+    </li>
+  );
+}
