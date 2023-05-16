5325f0eb847800e018be805b116a113a4de5fff8
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-16 18:32:01 +0200

    work

---
+++ src/app/components/react-soundcloud/SoundCloudReact.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
+import classes from '@/app/components/react-soundcloud/SOundCloudReact.module.css';
+
+export default function SoundCloudReact() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  return (
+    <>
+      {isLoaded ? (
+        <div className={classes.playerWrapper}>
+          <ReactPlayer
+            url='https://soundcloud.com/adrianandrea'
+            playing={false}
+            className={classes.reactPlayer}
+            width='100%'
+            height='100%'
+          />
+        </div>
+      ) : null}
+    </>
+  );
+}
