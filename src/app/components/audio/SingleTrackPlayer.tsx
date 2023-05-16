c9bf6435cbefa2407aec24ee892d7c34d943ad10
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-16 13:16:16 +0200

    work

--- src/app/components/audio/SingleTrackPlayer.tsx
+++ src/app/components/audio/SingleTrackPlayer.tsx
@@ -1,100 +1,92 @@
 'use client';
 
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
+import AudioControls from '@/app/components/audio/AudioControls';
+import AudioTime from '@/app/components/audio/AudioTime';
 import AudioProgress from '@/app/components/audio/AudioProgress';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlayCircle, faPauseCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function SingleTrackPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
-  const [progress, setProgress] = useState(0);
 
   const audioRef = useRef<HTMLAudioElement>(null);
   const progressBarRef = useRef<HTMLInputElement>(null);
+  const animationRef = useRef<number>();
 
-  function handlePlay() {
+  const changePlayerCurrentTime = useCallback(() => {
+    if (progressBarRef.current) {
+      progressBarRef.current.style.setProperty(
+        '--seek-before-width',
+        `${(+progressBarRef.current.value / duration) * 100}%`
+      );
+      setCurrentTime(+progressBarRef.current.value);
+    }
+  }, [duration]);
+
+  const whilePlaying = useCallback(() => {
+    if (progressBarRef.current && audioRef.current) {
+      progressBarRef.current.value = audioRef.current.currentTime.toString();
+      changePlayerCurrentTime();
+      animationRef.current = requestAnimationFrame(whilePlaying);
+    }
+  }, [changePlayerCurrentTime]);
+
+  const handlePlay = useCallback(() => {
     if (audioRef.current) audioRef.current.play();
-  }
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  }, [whilePlaying]);
 
   function handlePause() {
     if (audioRef.current) audioRef.current.pause();
+    if (animationRef.current) cancelAnimationFrame(animationRef.current);
   }
 
-  function handleCurrentTime() {
-    if (audioRef.current) setCurrentTime(audioRef.current.currentTime);
-  }
-
-  function calculateTime(secs: number) {
-    const minutes = Math.floor(secs / 60);
-    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    const seconds = Math.floor(secs % 60);
-    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${returnedMinutes} : ${returnedSeconds}`;
+  function handleChangeRange() {
+    if (audioRef.current && progressBarRef.current) {
+      audioRef.current.currentTime = +progressBarRef.current.value;
+      changePlayerCurrentTime();
+    }
   }
 
   useEffect(() => {
     isPlaying ? handlePlay() : handlePause();
-  }, [isPlaying]);
+  }, [isPlaying, handlePlay]);
 
   useEffect(() => {
     if (audioRef.current && progressBarRef.current) {
       const seconds = Math.floor(audioRef.current.duration);
       setDuration(seconds);
-
-      if (progress >= 100) {
-        setIsPlaying(false);
-        audioRef.current.currentTime = 0;
-      }
+      progressBarRef.current.max = seconds.toString();
     }
-  }, [audioRef?.current?.onloadedmetadata, audioRef?.current?.readyState, progress]);
+  }, [audioRef?.current?.onloadedmetadata, audioRef?.current?.readyState]);
 
   useEffect(() => {
-    setProgress((currentTime / duration) * 100);
+    if (currentTime === duration) {
+      setIsPlaying(false);
+      if (audioRef.current && progressBarRef.current) {
+        audioRef.current.currentTime = 0;
+        progressBarRef.current.value = '0';
+        setCurrentTime(0);
+      }
+    }
   }, [currentTime, duration]);
-
-  let playPauseControls;
-
-  if (!isPlaying) {
-    playPauseControls = (
-      <FontAwesomeIcon
-        icon={faPlayCircle}
-        className='text-4xl text-red-600'
-        onClick={() => setIsPlaying(true)}
-      />
-    );
-  } else {
-    playPauseControls = (
-      <FontAwesomeIcon
-        icon={faPauseCircle}
-        className='text-4xl text-red-600'
-        onClick={() => setIsPlaying(false)}
-      />
-    );
-  }
 
   return (
     <>
-      <div className='mt-6 flex items-center'>
-        <audio
-          src='/avex.mp3'
-          id='audio'
-          ref={audioRef}
-          onTimeUpdate={handleCurrentTime}
-        />
-        {playPauseControls}
+      <div className='mt-4 flex items-center'>
+        <audio src='/avex.mp3' id='audio' ref={audioRef} />
+        <AudioControls isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
         <p className='ml-4 text-xs'>Bunker (Original Mix) -M5</p>
       </div>
-      <div className='progress'>
-        <span className='time current'>{calculateTime(currentTime)}</span>
-        <AudioProgress ref={progressBarRef} progress={progress} />
-        {/*  <div className='relative h-2 w-3/6 bg-red-500' ref={progressBarRef}>
-          <div className='absolute h-2 bg-white' style={{ width: progress + '%' }}></div>
-        </div> */}
-        <span className='time'>
-          {duration && !isNaN(duration) && calculateTime(duration)}
-        </span>
+      <div className='mt-4 w-4/5  sm:w-3/5 md:w-2/5 lg:w-1/4'>
+        <AudioProgress ref={progressBarRef} onChange={handleChangeRange} />
+      </div>
+
+      <div className='mt-2 flex gap-2'>
+        <AudioTime time={currentTime} />
+        <p className='text-xs'>/</p>
+        {duration && !isNaN(duration) && <AudioTime time={duration} />}
       </div>
     </>
   );
