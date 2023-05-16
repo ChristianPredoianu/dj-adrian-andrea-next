96393932844c62be4a2cae0db036686639ca031e
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-16 14:18:50 +0200

    work

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -7,11 +7,10 @@ export default function HeroSection() {
   return (
     <>
       <div
-        className={`relative flex min-h-screen w-screen items-center bg-[url(/hero-background.jpg)] bg-cover bg-center bg-no-repeat`}
+        className={`relative flex min-h-screen items-center bg-[url(/hero-background.jpg)] bg-cover bg-center bg-no-repeat `}
       >
-        <div className='fixed inset-0 min-h-screen bg-black opacity-70'></div>
-
-        <section className='container relative mx-auto mt-20 flex flex-col gap-10 px-4 pb-10 text-slate-200  md:gap-24 md:pb-0'>
+        <div className='absolute inset-0 min-h-screen bg-black opacity-70 '></div>
+        <section className='container relative mx-auto mt-20 flex flex-col gap-10 px-4 pb-10 text-slate-200 md:gap-24 md:pb-0'>
           <div className='md:w-3/6'>
             <h1 className='via-42.19% inline-block bg-gradient-to-b from-red-from-gradient/0 from-0% via-red-via-gradient to-red-to-gradient to-100% bg-clip-text text-4xl font-bold text-transparent  lg:text-6xl'>
               Adrian Andrea
@@ -29,7 +28,7 @@ export default function HeroSection() {
             <SingleTrackPlayer />
           </div>
         </section>
-        <div className='absolute bottom-0 right-0  z-50 w-2/5 md:w-3/12'>
+        <div className='absolute bottom-0 right-0 z-50 w-2/5 md:w-3/12'>
           <Image src={ProfileImg} className='w-full' alt='dj adrian andrea' />
         </div>
       </div>
