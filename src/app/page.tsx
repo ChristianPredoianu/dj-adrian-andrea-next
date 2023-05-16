9bbff6e03977bb11db8d837cc8325ec42b987340
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-16 18:32:11 +0200

    work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/app/components/sections/HeroSection';
 import AboutSection from '@/app/components/sections/AboutSection';
+import TracksSection from '@/app/components/sections/TracksSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <TracksSection />
     </>
   );
 }
