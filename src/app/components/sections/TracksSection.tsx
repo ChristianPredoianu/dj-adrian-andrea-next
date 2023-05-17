8eea9866a589454de09b37a38af48431d5e398e9
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-17 16:33:08 +0200

    work

--- src/app/components/sections/TracksSection.tsx
+++ src/app/components/sections/TracksSection.tsx
@@ -1,11 +1,16 @@
-import SoundCloudReact from '@/app/components/react-soundcloud/SoundCloudReact';
+import TrackCard from '@/app/components/cards/TrackCard';
+import { tracksArr } from '@/app/utils/tracks';
+
+console.log(tracksArr[0].img);
 
 export default function TracksSections() {
+  const tracks = tracksArr.map((track) => <TrackCard key={track.id} track={track} />);
+
   return (
-    <section className='bg-zinc-200 py-20'>
-      <div className='container mx-auto px-4 '>
-        <h3 className='mb-10 text-3xl text-orange-600'>Soundcloud</h3>
-        <SoundCloudReact />
+    <section className='bg-zinc-100  text-slate-200'>
+      <div className='container mx-auto px-4 py-16 md:py-32'>
+        <h3 className='mb-10  text-3xl text-orange-600'>Tracks</h3>
+        <div className='flex flex-wrap items-center justify-between gap-10'>{tracks}</div>
       </div>
     </section>
   );
