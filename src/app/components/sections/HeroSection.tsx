137c641139ebfee193fa693af89607fe5c199979
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-23 19:58:49 +0200

    work

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -1,14 +1,34 @@
 'use client';
 
 import Image from 'next/image';
-import { useContext, useEffect } from 'react';
+import { useContext, useRef, useLayoutEffect } from 'react';
 import { PlayerContext } from '@/app/store/PlayerContext';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import ProfileImg from '@/app/assets/images/adrian-andrea.png';
 import CtaBtn from '@/app/components/buttons/CtaBtn';
 import SingleTrackPlayer from '@/app/components/audio/SingleTrackPlayer';
 
 export default function HeroSection() {
   const { activePlayerId, setActivePlayerId } = useContext(PlayerContext);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const profileImgRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const profileImg = profileImgRef.current;
+
+    let ctx = gsap.context(() => {
+      gsap.from(profileImg, {
+        opacity: 0,
+        duration: 3,
+        ease: 'power0.out',
+      });
+    }, profileImgRef);
+
+    return () => ctx.revert();
+  }, []);
 
   const heroTrack = {
     url: 'https://docs.google.com/uc?export=download&id=1DKVRyOZTK49whnQKBdM-12cMTj7BcLv3',
@@ -23,7 +43,7 @@ export default function HeroSection() {
         <div className='absolute inset-0 min-h-screen bg-black opacity-70 '></div>
         <section className='container relative mx-auto mt-20 flex flex-col gap-10 px-4 pb-10 text-slate-200 md:gap-24 md:pb-0'>
           <div className='md:w-3/6'>
-            <h1 className='via-42.19% inline-block bg-gradient-to-b from-red-from-gradient/0 from-0% via-red-via-gradient to-red-to-gradient to-100% bg-clip-text text-4xl font-bold text-transparent  lg:text-6xl'>
+            <h1 className='via-42.19% inline-block bg-gradient-to-b from-red-from-gradient/0 from-0% via-red-via-gradient to-red-to-gradient to-100% bg-clip-text text-4xl font-bold text-transparent lg:text-6xl'>
               Adrian Andrea
             </h1>
             <p className='mt-6 text-sm md:text-xl'>
@@ -46,7 +66,12 @@ export default function HeroSection() {
           </div>
         </section>
         <div className='absolute bottom-0 right-0 z-50 w-2/5 md:w-3/12'>
-          <Image src={ProfileImg} className='w-full' alt='dj adrian andrea' />
+          <Image
+            src={ProfileImg}
+            className='w-full'
+            alt='dj adrian andrea'
+            ref={profileImgRef}
+          />
         </div>
       </div>
     </>
