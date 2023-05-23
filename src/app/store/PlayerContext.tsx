3ea6ffb02d04501faef216b9dc4c2d08cd7a19fa
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-23 16:08:50 +0200

    added context

---
+++ src/app/store/PlayerContext.tsx
@@ -0,0 +1,37 @@
+'use client';
+import React, { createContext, useState, useCallback, useEffect } from 'react';
+
+interface PlayerContextProps {
+  activePlayerId: number | null;
+  setActivePlayerId: (id: number | null) => void;
+}
+
+export const PlayerContext = createContext<PlayerContextProps>({
+  activePlayerId: null,
+  setActivePlayerId: () => {},
+});
+
+interface PlayerContextProviderProps {
+  children: React.ReactNode;
+}
+
+export const PlayerContextProvider: React.FC<PlayerContextProviderProps> = ({
+  children,
+}) => {
+  const [activePlayerId, setActivePlayerId] = useState<number | null>(null);
+
+  const handleSetActivePlayerId = useCallback((id: number | null) => {
+    setActivePlayerId(id);
+  }, []);
+
+  useEffect(() => {
+    console.log(activePlayerId);
+  });
+
+  const contextValue: PlayerContextProps = {
+    activePlayerId,
+    setActivePlayerId: handleSetActivePlayerId,
+  };
+
+  return <PlayerContext.Provider value={contextValue}>{children}</PlayerContext.Provider>;
+};
