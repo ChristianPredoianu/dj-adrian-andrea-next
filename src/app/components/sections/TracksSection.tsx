42e03d10e8e707cf06d62b1f54ad7cfc448a4939
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-23 16:10:10 +0200

    work

--- src/app/components/sections/TracksSection.tsx
+++ src/app/components/sections/TracksSection.tsx
@@ -1,30 +1,29 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useContext } from 'react';
+import { PlayerContext } from '@/app/store/PlayerContext';
 import TrackCard from '@/app/components/cards/TrackCard';
 import { tracksArr } from '@/app/utils/tracks';
 
 export default function TracksSections() {
-  const [isActivePlaying, setIsActivePlaying] = useState(0);
-
-  const activePlayerHandler = useCallback((id: number) => {
-    setIsActivePlaying(id);
-  }, []);
+  const { activePlayerId, setActivePlayerId } = useContext(PlayerContext);
 
   const tracks = tracksArr.map((track) => (
     <TrackCard
       key={track.id}
       track={track}
-      activePlayerHandler={() => activePlayerHandler(track.id)}
-      playing={isActivePlaying === track.id}
+      activePlayerHandler={() => setActivePlayerId(track.id)}
+      isActive={activePlayerId === track.id}
     />
   ));
 
   return (
     <section className='bg-zinc-100  text-slate-200'>
       <div className='container mx-auto px-4 py-16 md:py-32'>
-        <h3 className='mb-10  text-3xl text-orange-600'>Tracks</h3>
-        <div className='flex flex-wrap items-center justify-between gap-10'>{tracks}</div>
+        <h3 className='mb-10 text-3xl text-slate-800'>Tracks</h3>
+        <div className='flex flex-wrap items-center justify-center gap-10 md:gap-20 lg:justify-between'>
+          {tracks}
+        </div>
       </div>
     </section>
   );
