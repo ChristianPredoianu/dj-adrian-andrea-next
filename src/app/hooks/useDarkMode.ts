6a9f26aaf7f508ada385a87b3bbd840b58a061f2
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-24 13:11:57 +0200

    added dark mode hook

---
+++ src/app/hooks/useDarkMode.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export default function useDarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Check the initial theme preference
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    setIsDarkMode(prefersDarkMode);
+
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    function handleDarkModeChange(e: MediaQueryListEvent) {
+      setIsDarkMode(e.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleDarkModeChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleDarkModeChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+
+    if (isDarkMode) {
+      htmlElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      htmlElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDarkMode]);
+
+  return { isDarkMode, setIsDarkMode };
+}
