54e03e2062900001325cd358173843c584858209
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-30 14:32:54 +0200

    work

--- src/app/components/cards/TrackCard.tsx
+++ src/app/components/cards/TrackCard.tsx
@@ -1,5 +1,6 @@
-import { forwardRef } from 'react';
+import { forwardRef, useContext } from 'react';
 import Image from 'next/image';
+import { PlayerContext } from '@/app/store/PlayerContext';
 import SingleTrackPlayer from '@/app/components/audio/SingleTrackPlayer';
 
 interface TrackCardProps {
@@ -9,15 +10,16 @@ interface TrackCardProps {
     url: string;
     track: string;
     alt: string;
+    id: number;
   };
-  activePlayerHandler: () => void;
-  isActive: boolean;
 }
 
 export default forwardRef<HTMLElement, TrackCardProps>(function TrackCard(
-  { track, isActive, activePlayerHandler },
+  { track },
   ref
 ) {
+  const { activePlayerId, setActivePlayerId } = useContext(PlayerContext);
+
   return (
     <article
       className='card flex w-4/5 flex-col bg-slate-950 sm:w-3/5 lg:w-2/5 2xl:flex-row'
@@ -32,15 +34,15 @@ export default forwardRef<HTMLElement, TrackCardProps>(function TrackCard(
           className='relative object-cover'
         />
       </div>
-      <div className='items-center flex w-full flex-col bg-slate-950 py-10 shadow-2xl'>
+      <div className='flex w-full flex-col items-center bg-slate-950 py-10 shadow-2xl dark:bg-sky-950'>
         <h3 className='text-center text-xl'>{track.artist}</h3>
         <h4 className='mt-2 text-gray-300'>{track.track}</h4>
-        <div className='items-center mt-10 flex w-full flex-col justify-center'>
+        <div className='flex w-full flex-col items-center justify-center'>
           <SingleTrackPlayer
             track={track}
             isTracksSection={true}
-            activePlayerHandler={activePlayerHandler}
-            isActive={isActive}
+            activePlayerHandler={() => setActivePlayerId(track.id)}
+            isActive={activePlayerId === track.id}
           />
         </div>
       </div>
