85711439e3b4de0a959ee40c3f5333324ded5674
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-30 14:33:11 +0200

    work

--- src/app/components/audio/SingleTrackPlayer.tsx
+++ src/app/components/audio/SingleTrackPlayer.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useRef, useState, useEffect, useCallback } from 'react';
-/* import AudioControls from '@/app/components/audio/AudioControls'; */
+import { useState, useEffect } from 'react';
 import AudioVisualizer from '@/app/components/audio/audio-visualizer/AudioVisualizer';
 import classNames from 'classnames';
 
@@ -24,48 +23,37 @@ export default function SingleTrackPlayer({
 }: SingleTrackPlayerProps) {
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const audioRef = useRef<HTMLAudioElement>(null);
-
-  const handlePlay = useCallback(() => {
-    /*    audioRef.current.play(); */
-    activePlayerHandler();
-  }, [activePlayerHandler]);
-
-  function handlePause() {
-    if (audioRef.current) audioRef.current.pause();
-  }
-
   useEffect(() => {
     isActive ? setIsPlaying(true) : setIsPlaying(false);
   }, [isActive]);
 
-  useEffect(() => {
-    isPlaying ? handlePlay() : handlePause();
-  }, [isPlaying, handlePlay]);
-
   return (
     <>
-      <div className=' items-center mt-4 flex '>
-        {/*  {track && <audio src={track.url} id='audio' ref={audioRef} />} */}
-        {/*  <AudioControls isPlaying={isPlaying} setIsPlaying={setIsPlaying} /> */}
-        {!isTracksSection && track && (
-          <p className='ml-4 text-xs'>{`${track.track} -${track.artist}`}</p>
-        )}
-      </div>
       <div
         className={classNames('mt-4 ', {
           'w-full': isTracksSection,
           'w-4/5 sm:w-3/5 md:w-2/5 lg:w-1/4': !isTracksSection,
         })}
       ></div>
-      <div className='mt-6 w-full'>
-        <AudioVisualizer
-          track={track.url}
-          isPlaying={isPlaying}
-          setIsPlaying={setIsPlaying}
-          handlePlay={handlePlay}
-          handlePause={handlePause}
-        />
+      <div
+        className={classNames('mt-6 w-full md:w-3/4', {
+          'w-full md:w-full': isTracksSection,
+        })}
+      >
+        <div className='relative mt-6'>
+          <div className='absolute -top-2 left-8 mt-4'>
+            {!isTracksSection && track && (
+              <p className='ml-4 text-xs'>{`${track.track} -${track.artist}`}</p>
+            )}
+          </div>
+          <AudioVisualizer
+            track={track.url}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            activePlayerHandler={activePlayerHandler}
+            isTracksSection={isTracksSection}
+          />
+        </div>
       </div>
     </>
   );
