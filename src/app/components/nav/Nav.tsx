410bd63d3fe5141b89cb901a687ba10d470f3ed3
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-30 14:32:25 +0200

    work

--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -29,13 +29,16 @@ export default function Nav() {
       <header className='container absolute left-0 right-0 z-50 m-auto w-full px-4 py-4 lg:py-0'>
         <nav className='relative flex flex-wrap items-center justify-between'>
           <NavLogo />
-          <DarkModeToogler />
+
+          <div className='xl:-ml-72'>
+            <DarkModeToogler />
+          </div>
           <Hamburger isNavbarOpen={isNavbarOpen} onToggleNavbar={toggleNavbarHandler} />
           <div
             className={`items-center  lg:flex
            ${isNavbarOpen ? ' flex' : ' hidden'}`}
           >
-            <ul className='fixed left-0 top-0 z-30 flex h-screen w-full flex-col items-center justify-center gap-10 bg-red-800 px-2 py-10 lg:static lg:ml-auto lg:h-auto lg:w-auto lg:flex-row lg:gap-0 lg:bg-transparent '>
+            <ul className='fixed left-0 top-0 z-30 flex h-screen w-full flex-col items-center justify-center gap-10 bg-slate-950 px-2 py-10 lg:static lg:h-auto lg:w-auto lg:flex-row lg:gap-0 lg:bg-transparent xl:-ml-56 '>
               {navLinks}
             </ul>
           </div>
