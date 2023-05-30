aefef6ef15961bd43cdaecfde8253c448dfba2cd
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-30 14:31:29 +0200

    work

--- src/app/components/sections/TracksSection.tsx
+++ src/app/components/sections/TracksSection.tsx
@@ -1,32 +1,33 @@
 'use client';
 
-import { useContext, useLayoutEffect, useRef } from 'react';
-import { PlayerContext } from '@/app/store/PlayerContext';
+import { useLayoutEffect, useRef } from 'react';
+
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import TrackCard from '@/app/components/cards/TrackCard';
 import { tracksArr } from '@/app/utils/tracks';
 
 export default function TracksSections() {
-  const { activePlayerId, setActivePlayerId } = useContext(PlayerContext);
-
   gsap.registerPlugin(ScrollTrigger);
 
-  const cardContainerRef = useRef<HTMLDivElement>(null);
+  const sectionRef = useRef(null);
+  const headingRef = useRef(null);
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      let cards = gsap.utils.selector(cardContainerRef);
+      let cards = gsap.utils.selector(sectionRef);
+      const heading = headingRef.current;
 
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: cardContainerRef.current,
+          trigger: sectionRef.current,
           scrub: 1,
           start: '0 80%',
           end: 'bottom 100%',
         },
       });
 
+      tl.from(heading, { ease: 'power0.out', y: 100 });
       tl.from(cards('.card'), {
         stagger: 3,
         duration: 3,
@@ -35,27 +36,22 @@ export default function TracksSections() {
         scale: 0.9,
         ease: 'power0',
       });
-    }, cardContainerRef);
+    }, sectionRef);
     return () => ctx.revert();
   }, []);
 
-  const tracks = tracksArr.map((track) => (
-    <TrackCard
-      key={track.id}
-      track={track}
-      activePlayerHandler={() => setActivePlayerId(track.id)}
-      isActive={activePlayerId === track.id}
-    />
-  ));
+  const tracks = tracksArr.map((track) => <TrackCard key={track.id} track={track} />);
 
   return (
-    <section className='bg-zinc-100 text-slate-200 dark:bg-slate-400'>
+    <section className='bg-zinc-200 text-slate-200 dark:bg-stone-950' ref={sectionRef}>
       <div className='container mx-auto px-4 py-16 md:py-32'>
-        <h3 className='mb-10 text-3xl text-slate-800'>Tracks</h3>
-        <div
-          className='flex flex-wrap items-center justify-center gap-10 md:gap-20 lg:justify-between'
-          ref={cardContainerRef}
+        <h3
+          className='mb-10 text-3xl text-slate-800 dark:text-slate-300'
+          ref={headingRef}
         >
+          Tracks
+        </h3>
+        <div className='flex flex-wrap items-center justify-center gap-10 md:gap-20 lg:justify-between'>
           {tracks}
         </div>
       </div>
