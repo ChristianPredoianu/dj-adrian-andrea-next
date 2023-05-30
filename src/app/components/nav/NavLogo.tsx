6bc7a3e40bbd84d33bc0dea80da0894cd69b8d44
Christian Predoianu <christianpredoianu@yahoo.com>
2023-05-30 14:32:04 +0200

    work

--- src/app/components/nav/NavLogo.tsx
+++ src/app/components/nav/NavLogo.tsx
@@ -1,7 +1,12 @@
 export default function NavLogo() {
   return (
-    <p className='text-1xl inline-block bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold  text-transparent'>
-      Dj Adrian Andrea
-    </p>
+    <div className='flex flex-col md:flex-row'>
+      <p className='text-1xl inline-block bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold  text-transparent'>
+        Dj Adrian
+      </p>
+      <p className='text-1xl bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold text-transparent md:ml-2'>
+        Andrea
+      </p>
+    </div>
   );
 }
