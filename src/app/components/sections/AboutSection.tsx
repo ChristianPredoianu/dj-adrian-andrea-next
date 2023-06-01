8ec2060a96e579c3b95afce2839d05c0eb96d54a
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-01 13:23:26 +0200

    final

--- src/app/components/sections/AboutSection.tsx
+++ src/app/components/sections/AboutSection.tsx
@@ -17,13 +17,15 @@ export default function AboutSection() {
   const socialMediaRef = useRef(null);
 
   useLayoutEffect(() => {
-    const img = imgRef.current;
+    let mm = gsap.matchMedia();
+
     const aboutHeading = aboutHeadingRef.current;
     const aboutSection = aboutSectionRef.current;
+    const img = imgRef.current;
     const paragraph = paragraphRef.current;
     const socialMedia = socialMediaRef.current;
 
-    let ctx = gsap.context(() => {
+    mm.add('(min-width: 1024px)', () => {
       const tl = gsap.timeline({
         defaults: { duration: 1 },
         scrollTrigger: {
@@ -38,12 +40,14 @@ export default function AboutSection() {
       tl.from(paragraph, { ease: 'power0.out', y: 100, opacity: 0 });
       tl.from(socialMedia, { ease: 'power0.out', y: 100, opacity: 0 });
     });
-    return () => ctx.revert();
+    return () => mm.revert();
   }, []);
+
   return (
     <section
-      className='bg-zinc-300 text-slate-800 dark:bg-neutral-900 dark:text-slate-300'
+      className='bg-primary-color text-font-dark dark:bg-primary-color-dark dark:text-font-light'
       ref={aboutSectionRef}
+      id='about'
     >
       <div className='container mx-auto px-4 py-16 md:py-32'>
         <h2 className='text-3xl' ref={aboutHeadingRef}>
@@ -54,27 +58,12 @@ export default function AboutSection() {
             <Image src={MixingImg} className='w-full' alt='dj mixing' />
           </div>
 
-          <p className='lg:w-fit' ref={paragraphRef}>
-            {`My name is Adrian Andrea, and I'm a DJ with a passion for music and a love for
-  entertaining. Ever since I was young, I've been fascinated by the power of
-  music to bring people together and create unforgettable experiences. From
-  classic rock to electronic dance music, I've always had an eclectic taste and
-  an open mind when it comes to exploring different genres. Over the years, I've
-  honed my skills as a DJ and developed a unique style that blends different
-  sounds and rhythms to create a vibe that's all my own. Whether I'm playing to
-  a packed dancefloor or setting the mood for a more intimate setting, I'm
-  always focused on creating an atmosphere that's fun, engaging, and
-  unforgettable. But being a DJ isn't just about playing music - it's about
-  connecting with people and creating a sense of community. That's why I always
-  make an effort to engage with my audience and create a sense of shared
-  experience. Whether it's through call-and-response chants, shoutouts, or just
-  having a conversation between sets, I'm always looking for ways to connect
-  with my listeners and make them feel like they're part of the experience.
-  Ultimately, my goal as a DJ is to spread the joy of music and bring people
-  together through the power of sound. Whether you're a die-hard fan or a
-  first-time listener, I hope you'll join me on this journey and experience the
-  magic for yourself. Thank you for visiting my website, and I can't wait to see
-  you on the dancefloor!`}
+          <p className='leading-8 lg:w-fit lg:text-lg' ref={paragraphRef}>
+            {`Adrian's background in music begins at an early age. At the age of seven he started playing drums. This led on to a specialized secondary school for young musicians.
+            His passion for music led him on to start spinning discs at the beginning of the millennium and he has played at various venues such as large clubs to private parties. After his little tour with Victoria's Secret Sweden, he moved to Barcelona to evolve his DJ'ing.
+            At this moment he has experience from Sweden, Romania, Spain, Algeria and Greece, where he had a residency on the sunny island of Rhodes and later returned to Malmö, Sweden where he is now one of the most reknown underground DJ's having played 'Basement', 'Nachtwaker', 'City to City' and 'Serious Fun'.
+            Adrian has been a resident DJ for almost two years on the world known radio station www.oneundergroundradio.com every sunday evening, and now also on Germany's biggest community for electronic music, http://www.w-baselounge.de/ where his fine sets are played on friday evenings.
+            Adrian's style is sophisticated in the genre of House and his style is rhythmic and groovy with a touch of funk, tribal, deep and tech.`}
           </p>
         </div>
         <div className='mt-10' ref={socialMediaRef}>
