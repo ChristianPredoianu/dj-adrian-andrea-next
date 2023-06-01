b472a6d596bbecc05f2299b1696064467ca42c7f
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-01 13:23:15 +0200

    final

--- src/app/components/sections/HeroSection.tsx
+++ src/app/components/sections/HeroSection.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import Image from 'next/image';
 import { useContext, useRef, useLayoutEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { PlayerContext } from '@/app/store/playerContext';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
@@ -41,7 +42,7 @@ export default function HeroSection() {
         className={`relative flex min-h-screen items-center bg-[url(/hero-background.jpg)] bg-cover bg-center bg-no-repeat `}
       >
         <div className='absolute inset-0 min-h-screen bg-slate-950 opacity-70 dark:bg-black'></div>
-        <section className='container relative mx-auto mt-20 flex flex-col gap-10 px-4 pb-10 text-slate-200 md:gap-24 md:pb-0'>
+        <section className='container relative mx-auto mt-20 flex flex-col gap-10 px-4 pb-10 text-font-light md:gap-24 md:pb-0'>
           <div className='md:w-3/6'>
             <h1 className='via-42.19% inline-block bg-gradient-to-b from-red-from-gradient/0 from-0% via-red-via-gradient to-red-to-gradient to-100% bg-clip-text text-4xl font-bold text-transparent lg:text-6xl'>
               Adrian Andrea
@@ -52,7 +53,9 @@ export default function HeroSection() {
               talent for creating unforgettable sets, Adrian is the go-to DJ for anyone
               looking to take their party to the next level.
             </p>
-            <CtaBtn />
+            <Link href={'#tracks'} scroll={false}>
+              <CtaBtn />
+            </Link>
           </div>
           <div>
             <p className='text-xs font-bold'>View All Tracks</p>
