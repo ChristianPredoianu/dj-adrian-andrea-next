cfe3be7d90028de44b154de478617ed1b0ba5621
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-01 13:21:24 +0200

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+'use client';
+
+import { useRef } from 'react';
 import HeroSection from '@/app/components/sections/HeroSection';
 import AboutSection from '@/app/components/sections/AboutSection';
 import TracksSection from '@/app/components/sections/TracksSection';
 import SoundCloudSection from '@/app/components/sections/SoundCloudSection';
 
 export default function Home() {
+  const heroSectionRef = useRef(null);
+  const aboutSectionRef = useRef(null);
+  const tracksSectionRef = useRef(null);
+
   return (
     <>
       <HeroSection />
       <AboutSection />
       <TracksSection />
-
-      {/* <SoundCloudSection /> */}
+      <SoundCloudSection />
     </>
   );
 }
