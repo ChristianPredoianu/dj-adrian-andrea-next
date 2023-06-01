847e0dfcc56e6aa0d7d25404c8e99e4bc6784083
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-01 13:23:35 +0200

    final

--- src/app/components/nav/NavLogo.tsx
+++ src/app/components/nav/NavLogo.tsx
@@ -1,10 +1,21 @@
-export default function NavLogo() {
+import classNames from 'classnames';
+
+interface NavLogoProps {
+  isNav: boolean;
+}
+
+export default function NavLogo({ isNav }: NavLogoProps) {
   return (
-    <div className='flex flex-col md:flex-row'>
-      <p className='text-1xl inline-block bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold  text-transparent'>
+    <div className={classNames('flex', { 'flex-col md:flex-row': isNav })}>
+      <p className='text-1xl inline-block bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold text-transparent'>
         Dj Adrian
       </p>
-      <p className='text-1xl bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold text-transparent md:ml-2'>
+      <p
+        className={classNames(
+          'text-1xl ml-2 bg-gradient-to-r from-gray-200 via-gray-400 to-gray-700 bg-clip-text font-bold text-transparent md:ml-2',
+          { 'ml-0 md:ml-2': isNav }
+        )}
+      >
         Andrea
       </p>
     </div>
