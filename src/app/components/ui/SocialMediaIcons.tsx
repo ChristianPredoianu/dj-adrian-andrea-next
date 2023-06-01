833745e6e1dad4c751f291882fd4b9396324f7db
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-01 13:22:43 +0200

    added hover animation + links

--- src/app/components/ui/SocialMediaIcons.tsx
+++ src/app/components/ui/SocialMediaIcons.tsx
@@ -9,9 +9,36 @@ import {
 export default function SocialMediaIcons() {
   return (
     <div className=' flex gap-4 text-2xl text-red-800'>
-      <FontAwesomeIcon icon={faSoundcloud} />
-      <FontAwesomeIcon icon={faInstagram} />
-      <FontAwesomeIcon icon={faFacebook} />
+      <a
+        href='https://soundcloud.com/adrianandrea'
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        <FontAwesomeIcon
+          icon={faSoundcloud}
+          className='transition-duration: 150ms cursor-pointer transition-colors hover:text-red-600'
+        />
+      </a>
+      <a
+        href='https://instagram.com/djadrianandrea/'
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        <FontAwesomeIcon
+          icon={faInstagram}
+          className='transition-duration: 150ms cursor-pointer transition-colors hover:text-red-600'
+        />
+      </a>
+      <a
+        href='https://www.facebook.com/djadrianandrea'
+        rel='noopener noreferrer nofollow'
+        target='_blank'
+      >
+        <FontAwesomeIcon
+          icon={faFacebook}
+          className='transition-duration: 150ms cursor-pointer transition-colors hover:text-red-600'
+        />
+      </a>
     </div>
   );
 }
