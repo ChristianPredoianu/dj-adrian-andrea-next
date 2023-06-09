b20c7847c8d2a3add9e7eff5beb3bc88d75c9cc9
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-09 12:07:52 +0200

    final

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -2,6 +2,35 @@ import NavLogo from '@/app/components/nav/NavLogo';
 import SocialMediaIcons from '@/app/components/ui/SocialMediaIcons';
 
 export default function Footer() {
+  const credits = (
+    <div className='mt-6 flex flex-col items-center md:flex-row md:justify-between'>
+      <div className='text-center text-xs text-gray-600'>
+        <p>Credits:</p>
+        <a
+          href='https://wavesurfer-js.org/'
+          rel='noopener noreferrer nofollow'
+          target='_blank'
+        >
+          <p className='transition-duration: 150ms mt-1 cursor-pointer transition-colors hover:text-red-800'>
+            WaveSurfer.js
+          </p>
+        </a>
+      </div>
+      <div className='mt-1 text-center text-xs text-gray-600'>
+        <p>Copyright &copy;</p>
+        <a
+          href='https://github.com/ChristianPredoianu'
+          rel='noopener noreferrer nofollow'
+          target='_blank'
+        >
+          <p className='transition-duration: 150ms mt-1 cursor-pointer transition-colors hover:text-red-800 md:mt-0'>
+            Christian Predoianu
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+
   return (
     <footer className='bg-black py-20'>
       <div className='container mx-auto px-4'>
@@ -9,32 +38,7 @@ export default function Footer() {
           <NavLogo isNav={false} />
           <SocialMediaIcons />
         </div>
-        <div className='mt-6 flex flex-col items-center md:flex-row md:justify-between'>
-          <div className='text-center text-xs text-gray-600'>
-            <p>Credits:</p>
-            <a
-              href='https://wavesurfer-js.org/'
-              rel='noopener noreferrer nofollow'
-              target='_blank'
-            >
-              <p className='transition-duration: 150ms mt-1 cursor-pointer transition-colors hover:text-red-800'>
-                WaveSurfer.js
-              </p>
-            </a>
-          </div>
-          <div className='mt-1 text-center text-xs text-gray-600'>
-            <p>Copyright &copy;</p>
-            <a
-              href='https://github.com/ChristianPredoianu'
-              rel='noopener noreferrer nofollow'
-              target='_blank'
-            >
-              <p className='transition-duration: 150ms mt-1 cursor-pointer transition-colors hover:text-red-800 md:mt-0'>
-                Christian Predoianu
-              </p>
-            </a>
-          </div>
-        </div>
+        {credits}
       </div>
     </footer>
   );
